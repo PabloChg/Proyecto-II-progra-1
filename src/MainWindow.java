9c826d00c8a8d1775fa5bfe6346666613646e5a2
Victor Steven Fernandez Espinoza <b72905@ecci.ucr.ac.cr@ECCI-LBL-104-13.ecci.ucr.ac.cr>
2017-11-16 11:22:26 -0600

    Changes after class

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,10 +1,10 @@
 import java.awt.*;
+import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.File;
-
 import javax.swing.*;
 
-public class MainWindow extends JFrame
+public class MainWindow extends JFrame implements MouseListener
 {
 	private JPanel positionTable;
 	public MainWindow() 
@@ -16,23 +16,55 @@ public class MainWindow extends JFrame
 		BorderLayout mainLayout = new BorderLayout();
 		this.setLayout( mainLayout );
 		
+		this.createIndicators();
 		this.createTable();	
-		this.createButtons();
-		
-
+		this.createButtons();		
 	}
+	
+	public void createIndicators()
+	{
+		JPanel indicators = new JPanel();
+		indicators.setLayout(new BorderLayout() );
+		
+		JLabel teams = new JLabel("Teams: #");
+		
+		indicators.add(teams, BorderLayout.NORTH);
+		
+		this.add(indicators, BorderLayout.NORTH);
+	}
+	
 	
 	public void createTable()
 	{
+		
 		Table table = new Table();
-		this.add(table, BorderLayout.NORTH);
+		
+		this.add(table, BorderLayout.CENTER);
 	}
+	
 	public void createButtons()
 	{		
 		JPanel buttons = new JPanel();
-		buttons.setLayout(new GridLayout(2,1));
+		buttons.setLayout(new BorderLayout() );
+		
 		JButton fileFinder = new JButton("Choose file");
-		this.add(fileFinder);
+		buttons.add(fileFinder, BorderLayout.WEST);
+		fileFinder.addMouseListener(this);
+		
+		JButton refreshButton = new JButton("Refresh");
+		buttons.add(refreshButton, BorderLayout.CENTER);
+		refreshButton.addMouseListener(this);
+		
+		
+		JButton closeButton = new JButton("Close");
+		buttons.add(closeButton, BorderLayout.EAST);
+		closeButton.addMouseListener(this);
+		
+		this.add(buttons, BorderLayout.SOUTH);
+	}
+
+	public void fileChooser()
+	{
 		// FileChooser
 		JFileChooser fileChooser = new JFileChooser();
 		int result = fileChooser.showOpenDialog(this);
@@ -41,10 +73,42 @@ public class MainWindow extends JFrame
 		    File selectedFile = fileChooser.getSelectedFile();
 		    System.out.println("Selected file: " + selectedFile.getAbsolutePath());
 		}
-
 	}
 
+	@Override
+	public void mouseClicked(MouseEvent e)
+	{
+		System.out.println("closed");
+		
+	}
 
+	@Override
+	public void mousePressed(MouseEvent e) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
 
 	
 
--- src/Table.java
+++ src/Table.java
@@ -1,5 +1,4 @@
 import java.awt.*;
-
 import javax.swing.*;
 import javax.swing.table.TableColumn;
 
@@ -9,49 +8,7 @@ import javax.swing.table.TableColumn;
 public class Table extends JPanel
 {	
 	JPanel positionTable = new JPanel();
-	
-	public void dTable() 
-	{
-		setLayout(new BorderLayout());
 		
-		positionTable.setLayout(new GridLayout(13,10));
-		
-		add(positionTable, BorderLayout.CENTER);
-		
-		addButtons("#");
-		addButtons("Equipo");
-		addButtons("PJ");
-		addButtons("V");
-		addButtons("E");
-		addButtons("D");
-		addButtons("GF");
-		addButtons("GC");
-		addButtons("DG");
-		addButtons("Pts");
-		
-		for (int index = 1; index < 13; index++) 
-		{
-			addButtons("# "+ index);
-			addButtons("Equipo " +index);
-			addButtons("-");
-			addButtons("-");
-			addButtons("-");
-			addButtons("-");
-			addButtons("-");
-			addButtons("-");
-			addButtons("-");
-			addButtons("-");
-
-		}
-		
-	}
-	
-	private void addButtons(String buttonName) 
-	{
-		JButton button = new JButton(buttonName);
-		this.positionTable.add(button);
-	}
-	
 	public Table()
 	{
 		String[] headers = {"#", "Equipo", "PJ", "PG",
@@ -59,10 +16,18 @@ public class Table extends JPanel
 		
 		String[][] teamStats = new String[13][13];
 		
+		for (int col = 0; col < 13; ++col)
+		{
+			for (int row = 0; row< 13; ++row)
+			{
+				teamStats[col][row] = "MANTEL";
+			}
+		}
+		
 		JTable table = new JTable(teamStats, headers);
 		
 		TableColumn column = null;
-		for (int col = 0; col < 5; col++) 
+		for (int col = 0; col < 10; col++) 
 		{
 			column = table.getColumnModel().getColumn(col);
 
